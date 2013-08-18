b8e81fe34de027e3385b455d3eaf8986e18827ed
Matt Cooper <vtbassmatt@gmail.com>
2013-08-18 16:54:16 -0700

    add player

--- ks.js
+++ ks.js
@@ -21,6 +21,16 @@
 		self.decrementScore = function(player) {
 			player.score(player.score() - 1);
 		}
+		
+		self.addPlayer = function() {
+			var newName = prompt("New player's name:","");
+			if (newName!=null && newName!="") {
+				self.players.push({
+					name: ko.observable(newName),
+					score: ko.observable(0)
+				});
+			}
+		}
 	}
 	
 	global.KeepScore = new keepscoreViewModel();
